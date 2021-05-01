16e3aa831e49b62ffd896db0ef9a448fe8d21b53
Алексей <tov.kessler@yandex.ru>
2021-05-01 21:52:22 +0800

    govno

--- src/components/RedactorMain/RedactorMain.tsx
+++ src/components/RedactorMain/RedactorMain.tsx
@@ -1,7 +1,79 @@
+import React, { useRef, useState } from 'react';
 import s from './RedactorMain.module.css';
 
+import { Stage, Layer, Rect, Line } from 'react-konva';
+
 export const RedactorMain = () => {
+  const [tool, setTool] = useState('pen');
+  const [lines, setLines] = useState([]);
+  const isDrawing = useRef(false);
+
+  const handleMouseDown = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    isDrawing.current = true;
+    const pos = e.target.getStage().getPointerPosition();
+    setLines([...lines, { tool, points: [pos.x, pos.y] }]);
+  };
+
+  const handleMouseMove = (e : React.MouseEvent<HTMLCanvasElement>) => {
+    // no drawing - skipping
+    if (!isDrawing.current) {
+      return;
+    }
+    const stage = e.target.getStage();
+    const point = stage.getPointerPosition();
+    let lastLine = lines[lines.length - 1];
+    // add point
+    lastLine.points = lastLine.points.concat([point.x, point.y]);
+
+    // replace last
+    lines.splice(lines.length - 1, 1, lastLine);
+    setLines(lines.concat());
+  };
+
+  const handleMouseUp = () => {
+    isDrawing.current = false;
+  };
+
   return (
-    <main className={s.main}>Main</main>
+    <main className={s.main}>
+      <Stage
+        width={500}
+        height={500}
+        onMouseDown={handleMouseDown}
+        onMousemove={handleMouseMove}
+        onMouseup={handleMouseUp}>
+        <Layer>
+          <Rect
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            fill="white"
+          />
+          {lines.map((line, i) => (
+            <Line
+              key={i}
+              points={line.points}
+              stroke="#df4b26"
+              strokeWidth={5}
+              tension={0.5}
+              lineCap="round"
+              globalCompositeOperation={
+                line.tool === 'eraser' ? 'destination-out' : 'source-over'
+              }
+            />
+          ))}
+        </Layer>
+      </Stage>
+      <select
+        value={tool}
+        onChange={(e) => {
+          setTool(e.target.value);
+        }}
+      >
+        <option value="pen">Pen</option>
+        <option value="eraser">Eraser</option>
+      </select>
+    </main>
   );
 };
